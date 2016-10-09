2ac49dab4f1f0a17f29b47e6841f7dcf9472b83e
Anthony Delgado <me@anthonydelgado.me>
2016-10-09 02:12:45 -0400

    Added logic to card input

--- public/assets/js/help.js
+++ public/assets/js/help.js
@@ -1,9 +1,27 @@
 $(document).on('click','#submit', function () {
-    var newPost = $('#textarea1').val('New Text');
-    $('#textarea1').trigger('autoresize');
-    $('#feed').append(); //newPost moves whole textbox to the bottom, need to create new card to prepend
+
+
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in.
+            var photoURL = user.photoURL;
+
+
+
+            var newPost = $('#textarea1').val();
+
+            console.log(newPost);
+
+
+            var newcard = '<div class="col s12 m8 offset-m2 l6 offset-l3"><div class="card-panel white z-depth-1"> <div class="row valign-wrapper">  <div class="col s2"> <img src="' + photoURL + '" alt="" class="circle responsive-img"> </div> <div class="col s10"> <span class="black-text"> ' + newPost + ' </span> </div> </div> </div> </div>';
+
+            $('#feed').prepend(newcard); //newPost moves whole textbox to the bottom, need to create new card to prepend
+
+
+        }
+    });
+
+
 });
-$(document).on('click', '#help', function(){
-    console.log('Clicked help');
-});
+
 
