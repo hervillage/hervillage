fb910d14ec1426b6ef6325e0d16f2e4c2b887634
Juanita Soranno <juanitasoranno@gmail.com>
2016-10-09 01:54:38 -0400

    Added cards

--- public/assets/js/help.js
+++ public/assets/js/help.js
@@ -1,2 +1,9 @@
-$('#textarea1').val('New Text');
-$('#textarea1').trigger('autoresize');
+$(document).on('click','#submit', function () {
+    var newPost = $('#textarea1').val('New Text');
+    $('#textarea1').trigger('autoresize');
+    $('#feed').append(); //newPost moves whole textbox to the bottom, need to create new card to prepend
+});
+$(document).on('click', '#help', function(){
+    console.log('Clicked help');
+});
+
